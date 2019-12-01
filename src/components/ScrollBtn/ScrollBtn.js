26b4721e759af72d7b5143eb1f9d879722fb77a2
Alyona Malinina <alenam2803@mail.ru>
2019-12-01 21:33:35 +0300

    added scroll button

--- src/components/ScrollBtn/ScrollBtn.js
+++ src/components/ScrollBtn/ScrollBtn.js
@@ -2,9 +2,36 @@ import React, { Component } from 'react';
 import './scrollBtn';
 
 export class ScrollBtn extends Component {
+  state = {
+    isShown: false,
+    scrollTop: 0,
+    scrollToTop: false
+  }
+
+  componentDidMount = () => {
+    window.addEventListener('scroll', () => {
+        this.setState({
+          isShown: window.pageYOffset >= 300,
+          scrollToTop: (this.state.scrollTop - window.pageYOffset) > 0,
+          scrollTop: window.pageYOffset
+        }) 
+    })
+  }
+
+  scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
+
   render(){
+    const style = {
+      display: this.state.isShown ? 'block' : 'none'
+    }
+
+    let classes = 'scrollBtn';
+    classes += this.state.scrollToTop ? ' scrollBtn-active' : '';
+
     return (
-      <div className='scrollBtn'>
+      <div className={classes} style={style} onClick={this.scrollToTop}>
         
       </div>
     )
